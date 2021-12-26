565d9fc9cd53fadcfedd5f3ba0b417da5e0d6160
Tsulukidze <55186715+osmanpampal28@users.noreply.github.com>
2021-12-26 04:34:07 -0800

    multi language
    Çoklu dil seçenekleri eklendi.

--- WebProjeOT/Controllers/BlogsController.cs
+++ WebProjeOT/Controllers/BlogsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -182,6 +184,18 @@ namespace WebProjeOT.Controllers
         {
             var context = _context.Blogs.Include(b => b.Categories).Include(b => b.Writer);
             return View(context);
+        }
+
+
+
+        // coklu dil
+        public IActionResult ChangeLanguage(string culture)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions() { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+
+            return Redirect(Request.Headers["Referer"].ToString());
         }
     }
 }
--- WebProjeOT/Startup.cs
+++ WebProjeOT/Startup.cs
@@ -3,13 +3,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using WebProjeOT.Data;
 using WebProjeOT.Models;
@@ -53,6 +57,49 @@ namespace WebProjeOT
             });
 
             services.AddControllersWithViews();
+
+
+            // coklu dil 
+            services.AddSingleton<LanguageService>();
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc()
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization(options =>
+                {
+                    options.DataAnnotationLocalizerProvider = (type, factory) =>
+                    {
+
+                        var assemblyName = new AssemblyName(typeof(ShareResource).GetTypeInfo().Assembly.FullName);
+
+                        return factory.Create("ShareResource", assemblyName.Name);
+
+                    };
+
+                });
+
+
+
+            services.Configure<RequestLocalizationOptions>(
+                options =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                        {
+                            new CultureInfo("en-US"),
+                            new CultureInfo("tr-TR"),
+                            
+                        };
+
+
+
+                    options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+
+                    options.SupportedCultures = supportedCultures;
+                    options.SupportedUICultures = supportedCultures;
+                    options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +118,10 @@ namespace WebProjeOT
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+
+            app.UseRequestLocalization(locOptions.Value);
 
             app.UseRouting();
 
